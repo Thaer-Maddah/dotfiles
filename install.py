bec88ae46286ec04330bf2613e74c383fe546f6e
Thaer Maddah <thaeronline@gmail.com>
2021-04-29 02:12:43 +0000

    create functional installation

--- install.py
+++ install.py
@@ -9,16 +9,8 @@ import os
 import sys
 import re
 import subprocess
+# from tqdm import tqdm
 from time import sleep
-
-# Search for file location
-homedir = os.environ['HOME']
-location_tag = 'File Location:'
-type_tag = 'File Type:'
-
-# Open file for read
-dir_path = os.getcwd()
-files = os.listdir(dir_path)
 
 
 def main():
@@ -31,10 +23,7 @@ def main():
                          "/" + "\033[091m\033[01m" + "No\033[0m]> ")
         if (response.upper() in "Y" or response.upper() in "YES") \
                 and response not in "":
-            # install()
-            # list1 = readContent()
-            # print(list1[2])
-            installFiles()
+            install()
         else:
             print("Process canceld by user!.")
             exit()
@@ -52,102 +41,9 @@ def checkSystem():
         raise SystemExit("Must be using Python 3")
 
 
-def readFiles():
-    # execluded python files and it's backup which  created by emacs
-    x = re.compile('^.[a-z_-]*[1-9]*.py(.)?$', re.IGNORECASE)
-    file_name, full_path_name, file_content = [], [], []
-    index = 0
-    for i in files:
-        if os.path.isfile(i) and not x.match(i):
-            file_name.append(i)
-            full_path_name.append(dir_path + "/" + i)
-            file_content.append(open(full_path_name[index], "r"))
-            index += 1
-    return file_name, full_path_name, file_content
-
-
-def readContent():
-    index = 0
-    counter = 0
-    file_location = []
-    file_dest = []
-    file_exists = []
-    rf = readFiles()
-    line = []
-    for lines in rf[2]:
-        lines = lines.readlines()
-        for line in lines:
-            counter += 1
-            if location_tag in line:
-                # print("Line{}: {}".format(counter, line.strip()))
-                # print(index, rf[0], "\t\t| File |")
-                index += 1
-                # get path from file and remove special characters
-                try:
-                    file_location = line.split("~")[1].strip()
-                    file_dest = homedir + file_location
-                    # check if file existes
-                    file_exists = os.path.exists(file_dest)
-
-                    # print(file_dest)
-                except FileNotFoundError:
-                    print(FileNotFoundError)
-                    # pass
-
-    return file_location, file_dest, file_exists
-
-
-def installFiles():
-    # files = readFiles()
-    data = readContent()
-    counter = 0
-
-    for i in data[0]:
-        print(data[0])
-        # file_exists = data[2]
-        # if file_exists:
-        #     try:
-        #         pass
-        #         # subprocess.run(['rm', data[1]])
-        #         # create links to the config files
-        #         res = subprocess.run(['ln', '-s',
-        #                               data[0],
-        #                               data[1]])
-        #         if res.returncode:
-        #             print("File not copied!.")
-        #         else:
-        #             # Initial call to print 0% progress
-        #             # total = 10
-        #             printProgressBar(0, 10,
-        #                              prefix='Progress:',
-        #                              suffix='Complete',
-        #                              length=30)
-
-        #             for i in range(10):
-        #                 # Do stuff...
-        #                 sleep(0.02)
-        #                 # Update Progress Bar
-        #                 printProgressBar(i + 1, 10,
-        #                                  prefix='Progress:',
-        #                                  suffix='Complete',
-        #                                  length=30)
-
-        #             print(f"File {f[0]} copied!")
-        #             # progress(f)
-
-        #     except IOError:
-        #         pass
-        #         # print('IOError encounterd')
-        # else:
-        #     # Check whether name is on PATH and marked as executable.
-        #     # from whichcraft import which
-        #     from shutil import which
-
-        #     if not which('emacs'):
-        #         print("Error")
-
-    else:
-        pass
+# def progress(file_name):
+    # for i in tqdm(range(1), ncols=75, desc="Copy " + file_name):
+    #     sleep(0.2)
 
 
 # Print iterations progress
@@ -174,6 +70,86 @@ def printProgressBar(iteration, total=10, prefix='', suffix='',
     # Print New Line on Complete
     if iteration == total:
         print()
+
+
+def install():
+    # Search for file location
+    homedir = os.environ['HOME']
+    location_tag = 'File Location:'
+
+    # Open file for read
+    dir_path = os.getcwd()
+    files = os.listdir(dir_path)
+
+    # execluded python files and it's backup
+    # that created by emacs or another text editor
+    x = re.compile('^.[a-z_-]*[1-9]*.py(.)?$', re.IGNORECASE)
+
+    index = 0
+
+    for file_name in files:
+        if os.path.isfile(file_name) and not x.match(file_name):
+            full_path_name = dir_path + "/" + file_name
+            file_content = open(full_path_name, "r")
+            index += 1
+            for line in file_content:
+                line = file_content.readline()
+                if location_tag in line:
+                    print(index, file_name, "\t\t| File |")
+
+                    # get path from file and remove special characters
+                    try:
+                        file_location = line.split("~")[1].strip()
+                        file_dest = homedir + file_location
+                        # check if file existes
+                        file_exist = os.path.exists(file_dest)
+                    except FileNotFoundError:
+                        print(FileNotFoundError)
+                        # pass
+
+                    if file_exist:
+                        try:
+                            # Remove destination file if exists
+                            subprocess.run(['rm', file_dest])
+                            # create links to the config files
+                            res = subprocess.run(['ln', '-s',
+                                                  full_path_name,
+                                                  file_dest])
+                            if res.returncode:
+                                print("File not copied!.")
+                            else:
+                                # Initial call to print 0% progress
+                                # total = 10
+                                printProgressBar(0, 10,
+                                                 prefix='Progress:',
+                                                 suffix='Complete',
+                                                 length=30)
+                                
+                                for i in range(10):
+                                    # Do stuff...
+                                    sleep(0.02)
+                                    # Update Progress Bar
+                                    printProgressBar(i + 1, 10,
+                                                     prefix='Progress:',
+                                                     suffix='Complete',
+                                                     length=30)
+
+                                print(f"File {file_name} copied!")
+                                # progress(f)
+
+                        except IOError:
+                            pass
+                            # print('IOError encounterd')
+                    else:
+                        """Check whether `name` is on PATH and marked as executable."""
+                        # from whichcraft import which
+                        from shutil import which
+
+                        return which(file_dest) is not None
+                        print("Error")
+
+                else:
+                    pass
 
 
 # Call main function

bbf49a2001bc228e1908c7ab2ee1efeeea3190c0
Thaer Maddah <thaeronline@gmail.com>
2022-04-19 21:25:54 +0300

    Add Go language preferences and reorganize ini.el

---
+++ emacs.d/elpa/go-guru-20181012.330/go-guru-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from go-guru.el  -*- no-byte-compile: t -*-
+(define-package "go-guru" "20181012.330" "Integration of the Go 'guru' analysis tool into Emacs." '((go-mode "1.3.1") (cl-lib "0.5")) :commit "fa2693278637f56759480d2bf203bb8aad107230" :keywords '("tools"))

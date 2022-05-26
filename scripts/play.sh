8a62d636a4205a2ae93b3c440da16a49de7b8784
Thaer Maddah <thaeronline@gmail.com>
2022-05-26 03:55:25 +0300

    update video player script

--- scripts/play.sh
+++ scripts/play.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 if [[ -z $1 ]]; then
     # Playing video
     du -a | cut -f2- | grep -iE "mp4$|mp3$|webm$|m4a$|wav$|ogg$|mkv$" | dmenu -i -l 30 -sb "#C95A49" | xargs -I "{}" mpv "{}" # || clear && dialog --msgbox "MPV does not support selected file format" 5 45'

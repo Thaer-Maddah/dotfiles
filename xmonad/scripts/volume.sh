4c12434f1ee7477a85da1747086ac520ec2336b8
Thaer Maddah <thaeronline@gmail.com>
2022-06-05 00:39:10 +0300

    improve script

--- xmonad/scripts/volume.sh
+++ xmonad/scripts/volume.sh
@@ -2,7 +2,8 @@
 #vol=$(amixer get Master | awk -F'[]%[]' '/%/ {if ($7 == "off") { print "MM" } else { print $2 }}' | head -n 1)
 
 #echo "Vol:$vol"
-
-[[ "$(pamixer --get-mute)" == "true" ]] && echo "Mute" || echo  "Vol:$(pamixer --get-volume)%"
+Vol=$(pamixer --get-volume)
+# [[ $Vol -gt 80 ]] && echo -e "high" || $Vol
+[[ "$(pamixer --get-mute)" == "true" ]] && echo "Mute" || echo  "Vol:$Vol%"
 
 exit 0

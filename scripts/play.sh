0ad6797a2124b93e3c3b919207c64881caaa477f
Thaer Maddah <thaeronline@gmail.com>
2022-07-30 18:00:53 +0300

    improving script

--- scripts/play.sh
+++ scripts/play.sh
@@ -7,7 +7,7 @@ ext="mp4$|mp3$|webm$|m4a$|wav$|ogg$|mkv$"
 # We must put else {condition} in curly braces to avoid subshell procces
 # first argument option for video or audio, n for audio anything else for video
 # second argument fot directory, leave it empty for current directory
-[[ $1 == "n" || $1 == "no" ]] && { option="--no-video"; menu_color="#C95000"; } || { option=""; menu_color="#C95A49"; }
+[[ $1 == "n" || $1 == "no" ]] && { option="--no-video"; menu_color="#C95000"; } || { option="--video-osd=no"; menu_color="#C95A49"; }
 [[ -n $2 ]] && file=$2 || { file="."; }
 # Playing video
 du -a "$file" | cut -f2- | grep -iE "$ext" | dmenu -i -l 30 -sb "$menu_color"  | xargs -I "{}" mpv "{}" "$option" # || clear && dialog --msgbox "MPV does not support selected file format" 5 45'

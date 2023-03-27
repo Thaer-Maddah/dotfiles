2a3a870c3399fc826a68bbb5cb5b4b525e896aa8
Thaer Maddah <thaeronline@gmail.com>
2023-03-27 11:43:03 +0300

    Screen Capture

---
+++ scripts/capture.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+dir="$HOME/Pictures/screenshot"
+choice=$(echo -e 'Screen\nWindow\nRegion' | dmenu -i -sb "#f76d23" )
+# focused=`swaymsg -t get_tree | jq '.. | (.nodes? // empty)[] | select(.focused and .pid) | .rect | "\(.x),\(.y) \(.width)x\(.height)"'`
+xy=$(hyprctl activewindow | grep at: | awk ' {print $2}')
+size=$(hyprctl activewindow | grep size | awk '{print $2}')
+
+case $choice in
+    Screen) slurp -o  | xargs -I "{}" grim -g "{}" $dir/screenshot_$(date +'%s.png');;
+    # Window) grim -g "$(eval echo $focused)" $dir/screenshot_$(date +'%s.png');;
+    Window) echo grim -g $xy $size $dir/screenshot_$(date +'%s.png');;
+    Region) grim -g "$(slurp -o)" $dir/screenshot_$(date +'%s.png');;
+esac
+

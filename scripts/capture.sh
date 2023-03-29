d5b53aa57b8f9fedcd92162a41538e287e1fc147
Thaer Maddah <thaeronline@gmail.com>
2023-03-30 01:56:41 +0300

    update configurations

--- scripts/capture.sh
+++ scripts/capture.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 dir="$HOME/Pictures/screenshot"
-choice=$(echo -e 'Screen\nWindow\nRegion' | dmenu -i -sb "#f76d23" )
+choice=$(echo -e 'Screen\nWindow\nRegion\nClipboard' | dmenu -i -sb "#f76d23" )
 # focused=`swaymsg -t get_tree | jq '.. | (.nodes? // empty)[] | select(.focused and .pid) | .rect | "\(.x),\(.y) \(.width)x\(.height)"'`
 xy=$(hyprctl activewindow | grep at: | awk ' {print $2}')
 size=$(hyprctl activewindow | grep size | awk '{print $2}')
@@ -11,5 +11,6 @@ case $choice in
     # Window) grim -g "$(eval echo $focused)" $dir/screenshot_$(date +'%s.png');;
     Window) echo grim -g $xy $size $dir/screenshot_$(date +'%s.png');;
     Region) grim -g "$(slurp -o)" $dir/screenshot_$(date +'%s.png');;
+    Clipboard) grim -g "$(slurp)" - | swappy -f -
 esac
 

e6ff7498950dbffab4252ed8bf2bfbde1bd7890a
Thaer-Maddah <thaeronline@gmail.com>
2023-04-02 07:06:28 +0300

    improve searching man pages with fzf

--- scripts/fman.sh
+++ scripts/fman.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#https://github.com/junegunn/fzf/wiki/examples
+
 # Searhing man pages with fzf
 function fman() {
     man -k . | fzf -q "$1" --prompt='man> '  --preview $'echo {} | tr -d \'()\' | awk \'{printf "%s ", $2} {print $1}\' | xargs -r man | col -bx | bat -l man -p --color always' | tr -d '()' | awk '{printf "%s ", $2} {print $1}' | xargs -r man

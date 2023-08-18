9922ab902dd543cedf59e5dc9df46a86db106178
Thaer-Maddah <thaeronline@gmail.com>
2023-08-18 18:36:47 +0300

    config script runs qemu

---
+++ qemu/run_audio.sh
@@ -0,0 +1,14 @@
+ #qemu-system-x86_64 --enable-kvm -boot order=d -drive file=void.img -m 3G -cpu host -vga virtio -display sdl,gl=on  -audiodev alsa,id=alsa,out.frequency=44100,out.channels=2,out.format=s16,out.mixing-engine=on -device ich9-intel-hda -device hda-duplex,audiodev=alsa & disowen
+ #
+ 
+ qemu-system-x86_64 --enable-kvm \
+	-boot order=d \
+ 	-m 4G \
+	-smp 2 \
+	-drive file=void.img \
+	-boot d \
+	-vga virtio \
+	-display sdl,gl=on \
+	-audiodev pa,id=snd0 -device ich9-intel-hda -device hda-output,audiodev=snd0
+	#-device AC97 \
+

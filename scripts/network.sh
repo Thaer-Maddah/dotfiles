762c2380f0cdc4528b0ed3fbc64167b59d6cad24
Thaer-Maddah <thaeronline@gmail.com>
2023-11-03 23:30:14 +0300

    Improving wifi message

--- scripts/network.sh
+++ scripts/network.sh
@@ -9,7 +9,7 @@ if [[ $wi =~ "wlp" ]]; then
     signal=$(iwconfig "$wi" | awk -F '=' '/Quality/ {print $2}' | cut -d '/' -f 1)
     bars=$((signal / 10))
     
-    case $bars in
+case $bars in
       0)     bar='[-----]' ;;
       1|2)   bar='[/----]' ;;
       3|4)   bar='[//---]' ;;
@@ -18,8 +18,18 @@ if [[ $wi =~ "wlp" ]]; then
       9|10)  bar='[/////]' ;;
       *)     bar='[--!--]' ;;
     esac
-    
-    echo "$essid $bar"
+     rx1=$(cat /sys/class/net/"$wi"/statistics/rx_bytes)   # Download rate
+    tx1=$(cat /sys/class/net/"$wi"/statistics/tx_bytes)   # Upload rate
+    while sleep 1; do
+        rx2=$(cat /sys/class/net/"$wi"/statistics/rx_bytes)
+        tx2=$(cat /sys/class/net/"$wi"/statistics/tx_bytes)
+        printf '%s : %s KB/s %s KB/s\n' "$essid $bar" "$(((rx2-rx1)/1024))" "$(((tx2-tx1)/1024))"
+        rx1=$rx2
+        tx1=$tx2
+    done
+
+   
+    #echo "$essid $bar"
     exit 0
 else
     # Wired Interface

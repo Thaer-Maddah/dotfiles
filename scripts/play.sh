7ead38c3aad768a79984337c2b7b932ee6908b86
Thaer-Maddah <thaeronline@gmail.com>
2025-12-21 05:14:01 +0300

    Improve the video playback

--- scripts/play.sh
+++ scripts/play.sh
@@ -2,13 +2,13 @@
 
 # File extension
 ext="mp4$|mp3$|webm$|m4a$|wav$|ogg$|mkv$"
+# ext="${file##*.}"
 #file=$PWD
-
 # We must put else {condition} in curly braces to avoid subshell procces
 # first argument option for video or audio, n for audio anything else for video
 # second argument fot directory, leave it empty for current directory
 [[ $1 == "n" || $1 == "no" ]] && { option="--no-video"; menu_color="#C95000"; } || { option="--video-osd=no"; menu_color="#C95A49"; }
 [[ -n $2 ]] && file=$2 || { file="."; }
 # Playing video
-du -a "$file" | cut -f2- | grep -iE "$ext" | dmenu -i -l 30 -sb "$menu_color"  | xargs -I {} mpv {} "$option" #1>/dev/null 2>/dev/null & # Run video and diswoen mpv # || clear && dialog --msgbox "MPV does not support selected file format" 5 45'
+du -a "$file" | cut -f2- | grep -iE "$ext" | dmenu -i -l 30 -sb "$menu_color"  | xargs -I {} mpv {} "$option" >/dev/null 2>1 & disown  # Run video and diswoen mpv # || clear && dialog --msgbox "MPV does not support selected file format" 5 45'
 

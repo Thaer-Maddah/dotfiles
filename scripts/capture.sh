95067a30141910e276d60b8b6b702e53aff6bd7a
Thaer-Maddah <thaeronline@gmail.com>
2025-12-27 08:51:24 +0300

    Optimize settings

--- scripts/capture.sh
+++ scripts/capture.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-dir="$HOME/Pictures/screenshot"
+dir="$HOME/Pictures/Screenshots"
 choice=$(echo -e 'Screen\nWindow\nRegion\nClipboard' | dmenu -i -sb "#f76d23" )
 # focused=`swaymsg -t get_tree | jq '.. | (.nodes? // empty)[] | select(.focused and .pid) | .rect | "\(.x),\(.y) \(.width)x\(.height)"'`
 xy=$(hyprctl activewindow | grep at: | awk ' {print $2}')
